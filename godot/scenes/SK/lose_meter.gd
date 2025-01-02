f46a73659e1a2c79c79aa6b4eec3f26bf0a2212d
kellersefi <keller.sefi@gmail.com>
2025-01-02 10:53:44 +0200

    added_win_screen

--- godot/scenes/SK/lose_meter.gd
+++ godot/scenes/SK/lose_meter.gd
@@ -4,12 +4,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var progress_bar = $ProgressBar
-	progress_bar.value = WinMeterState.win_state
+	progress_bar.value = LoseMeterState.lose_state
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var progress_bar = $ProgressBar
-	progress_bar.value = WinMeterState.win_state
+	progress_bar.value = LoseMeterState.lose_state
 	pass
--- godot/scenes/SK/win_meter.gd
+++ godot/scenes/SK/win_meter.gd
@@ -4,12 +4,14 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var progress_bar = $ProgressBar
-	progress_bar.value = LoseMeterState.lose_state
+	progress_bar.value = WinMeterState.win_state
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var progress_bar = $ProgressBar
-	progress_bar.value = LoseMeterState.lose_state
+	progress_bar.value = WinMeterState.win_state
+	if progress_bar.value >= progress_bar.max_value:
+		get_tree().change_scene_to_file("res://scenes/SK/win_screen.tscn")
 	pass
